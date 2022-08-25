0102ee389545b9b941e1d2b535e90fb263dba508
Bharat_Karle <70475700+Bharat-N-Karle@users.noreply.github.com>
2022-08-25 09:14:24 +0530

    Update gitdemo.c

--- gitdemo.c
+++ gitdemo.c
@@ -3,6 +3,7 @@ int main(){
 	printf("Hello World\n");
 	int a=4;
 	printf("The value of a");
+	printf("a+b=c");
 	printf("Welcome to the first assignment");
 	printf("Hi this is bharat");
 	printf("do job first");
