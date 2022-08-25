c288d128428d4f9b5a1de30c772033aa69127b4a
Bharat_Karle <70475700+Bharat-N-Karle@users.noreply.github.com>
2022-08-25 09:56:04 +0530

    Update gitdemo.c

--- gitdemo.c
+++ gitdemo.c
@@ -4,6 +4,7 @@ int main(){
 	int a=4;
 	printf("The value of a");
 	printf("a+b=c");
+	//this is devops assignment
 	printf("Time should be managed");
 	printf("Welcome to the first assignment");
 	printf("Hi this is bharat");
